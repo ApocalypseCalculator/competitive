f3a6f0f764b2d40f8a46532b9ce044cfeff84de8
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-17 18:40:51 -0400

    fastio template

---
+++ javafastiotemplate.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(in.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static String nextLine() throws IOException {
+        return in.readLine().trim();
+    }
+
+    public static void main(String[] args) throws IOException {
+        //code goes here
+    }
+}
