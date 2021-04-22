4ab1a1f5ed7b6060d3c0fd051c43922f95bc321f
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-21 21:12:50 -0400

    maintaining a sequence

---
+++ MCPT/MaintainingASequence.cpp
@@ -0,0 +1,21 @@
+#pragma GCC optimize "Ofast"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	unordered_set<int> vals;
+	for (int i = 0; i < n; i++) {
+		int v;
+		cin >> v;
+		if (!vals.insert(v).second) {
+			cout << "YES";
+			return 0;
+		}
+	}
+	cout << "NO";
+	return 0;
+}
