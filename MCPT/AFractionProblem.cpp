d0ab9b11bcb3e0c920f84718ace37b72dff90ffc
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-23 21:49:39 -0400

    a fraction problem

---
+++ MCPT/AFractionProblem.cpp
@@ -0,0 +1,13 @@
+#pragma GCC optimize "Ofast"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	long a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << a + c << " " << b + d;
+	return 0;
+}
+//This solution ACs
